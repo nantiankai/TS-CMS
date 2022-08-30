514bbed4f29e80ecc8a99987e5fe5b54086cde7d
nantiankai <nantiankai@haomo.ai>
2022-08-30 14:43:20 +0800

    搭建整体框架

---
+++ src/store/index.ts
@@ -0,0 +1,13 @@
+import { createStore } from 'vuex';
+
+const store = createStore({
+  state: () => {
+    return {
+      name: 'coderwhy'
+    };
+  },
+  mutations: {},
+  getters: {},
+  actions: {}
+});
+export default store;
